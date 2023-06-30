1cdb1d77e89c341ddfd91625211f429ddf34c3a6
grid013 <gridikore42@gmail.com>
2023-06-30 12:34:57 +0530

    Fixed - edit form issue and other changes

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def get_time(comment):
+    time = datetime.now()
+    # created_date = comment.created_at.astimezone(timezone('Asia/Kolkata'))
+    created_date = comment["createdate"]
+
+    start = datetime.strptime(created_date,'%Y-%m-%d %H:%M:%S')
+    end = datetime.strptime(datetime.strftime(time,'%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
+
+    diff_sec = (end -start).total_seconds()
+
+    seconds = diff_sec
+    minutes = diff_sec // 60
+    hours = (diff_sec // 60) // 60
+    days = ((diff_sec // 60) // 60) // 24
+
+    # print(days,minutes,hours,seconds)
+
+    if days > 0:
+        return str(int(days)) + " days ago"
+    elif hours > 0:
+        return str(int(hours)) + " hours ago"
+    elif minutes > 0:
+        return str(int(minutes)) + " minutes ago"
+    elif seconds > 0:
+        return str(int(seconds)) + " seconds ago"
+    else:
+        return "just now"
+
 def validate(email):
     userId = None
     result = runQuery("SELECT userid FROM users WHERE email = '" + email + "'","select")
@@ -40,7 +68,7 @@ def dashboard():
     if 'email' not in session:
         return redirect(url_for('root'))
     userData, loggedIn = makeSignin()
-    itemData = runQuery('SELECT * FROM bikes where status = 1'.format(userData["userid"]),"select")
+    itemData = runQuery('SELECT * FROM bikes where status = 1',"select")
 
     return render_template("dashboard.html", userData=userData, itemData=itemData, loggedIn=loggedIn)
 
@@ -81,20 +109,97 @@ def addBike():
 @app.route("/")
 def root():
     userData, loggedIn = makeSignin()
-    itemData = runQuery('SELECT * FROM bikes where status = 1 limit 2'.format(userData["userid"]),"select")
+    itemData = runQuery('SELECT * FROM bikes where status = 1 limit 2',"select")
     return render_template("index.html", userData = userData, loggedIn=loggedIn, itemData=itemData)
 
 @app.route("/bike_listing")
 def bike_listing():
     userData, loggedIn = makeSignin()
-    itemData = runQuery('SELECT * FROM bikes where status = 1'.format(userData["userid"]),"select")
+    itemData = runQuery('SELECT * FROM bikes where status = 1',"select")
     return render_template("bike_listing.html", userData = userData, loggedIn=loggedIn, itemData=itemData)
 
-@app.route("/bike_detail")
-def bike_detail():
+@app.route("/bike_detail/<bike_id>/", methods=["GET", "POST"])
+def bike_detail(bike_id):
     userData, loggedIn = makeSignin()
-    itemData = runQuery('SELECT * FROM bikes where status = 1'.format(userData["userid"]),"select")
-    return render_template("bike_detail.html", userData = userData, loggedIn=loggedIn, itemData=itemData)
+    if request.method == "POST":
+        
+        if loggedIn:
+            comment = request.form['comment']
+            ratings = request.form["stars"]
+            createDate = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+            try:
+                result = runQuery("INSERT INTO comment (userid, bikeid, ratings, message, createdate) VALUES ({}, {}, {}, '{}', '{}')".format(userData["userid"], bike_id, ratings, comment, createDate),"insert")
+                response = "Request is completed successfully."
+                return redirect(url_for('bike_detail'))
+            except:
+                response = "Request is failed."
+                return redirect(url_for('bike_detail', bike_id=bike_id))
+        else:
+            return redirect(url_for('bike_detail', bike_id=bike_id))
+
+    itemData = runQuery('SELECT * FROM bikes where bikeId = {} limit 1'.format(bike_id),"select")
+    commentsData = runQuery("SELECT cm.commentid,cm.message,cm.createdate,us.first_name from comment as cm left join users as us on us.userid = cm.userid where cm.bikeid = " + bike_id + " ORDER BY cm.commentid","select")
+    ratingsData = runQuery("SELECT round(avg(ratings),2) as avg_ratings,count(*) as tot_ratings,count(case ratings when 1 then 1 else null END) as one_rating,count(case ratings when 2 then 1 else null END) as two_rating,count(case ratings when 3 then 1 else null END) as three_rating,count(case ratings when 4 then 1 else null END) as four_rating,count(case ratings when 5 then 1 else null END) as five_rating from comment where bikeid = {}".format(bike_id),"select")
+    if len(ratingsData)>0:
+        ratingsData = ratingsData[0]
+
+    for comment in commentsData:
+        comment["date_diff"] = get_time(comment)
+    return render_template("bike_detail.html", userData = userData, commentsData=commentsData,ratingsData=ratingsData, loggedIn=loggedIn, bike=itemData[0])
+
+@app.route("/bikedelete/<bike_id>/", methods=["GET", "POST"])
+def deleteBike(bike_id=None):
+    if 'email' not in session:
+        return redirect(url_for('root'))
+
+    userData, loggedIn = makeSignin()
+
+    if request.method == 'POST':
+        try:
+            result = runQuery("UPDATE bikes SET status = {} WHERE bikeid = '{}'".format(0,bike_id),"update")
+            response = "Request is completed successfully."
+            return redirect(url_for('dashboard'))
+        except:
+            response = "Request is failed."
+            return redirect(url_for('dashboard'))
+
+@app.route("/bikedit/<bike_id>/", methods=["GET", "POST"])
+def editBook(bike_id=None):
+    if 'email' not in session:
+        return redirect(url_for('root'))
+
+    userData, loggedIn = makeSignin()
+
+    if request.method == 'GET':
+        productData = runQuery("SELECT * FROM bikes WHERE status = 1 and bikeid = " + bike_id +"","select")
+        return render_template("editBike.html", userData=userData ,data=productData[0], loggedIn=loggedIn)
+    elif request.method == 'POST':
+        file1 = request.files["image"]
+        if file1:
+            path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+            image = file1.filename
+            file1.save(path)
+        else:
+            image = request.form['image']
+
+
+        bikeName = request.form['bikeName']
+        description = request.form['description']
+        engine = request.form['engine']
+        mileage = request.form['mileage']
+        transmission = request.form['transmission']
+        price = request.form['price']
+        description = request.form['description']
+        createdate = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+        updatedate = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+
+        try:
+            result = runQuery("UPDATE bikes SET userid = {},image = '{}',bikeName = '{}',price = '{}',description = '{}', engine = '{}',mileage = '{}',transmission = '{}',createdate= '{}',updatedate= '{}' WHERE bikeid = '{}'".format(userData["userid"],image,bikeName,price,description,engine,mileage, transmission, createdate,updatedate,bike_id),"update")
+            response = "Request is completed successfully."
+            return redirect(url_for('dashboard'))
+        except:
+            response = "Request is failed."
+            return render_template("editBike.html",error=response)
 
 @app.route("/gallery")
 def gallery():
--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,45 @@
-function validateMyForm() {
+function validatecomment(loggedIn){
+    if (loggedIn == "True") {
+        document.getElementById("commentform").submit();
+    }
+    else {
+        alert("You have to login first")
+    }
+}
+function validateMyForm(loggedIn) {
     const days = document.getElementById("days").value;
-    const login = localStorage.getItem("login")
-    if(login == "true"){
+    if (loggedIn == "True") {
         let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'))
         modal.show();
     }
-    else{
-        alert("You have to login first")  
+    else {
+        alert("You have to login first")
     }
-
-    
 }
-
 function validateContactForm() {
     let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'))
     modal.show();
 }
 
-function logout() {
-    localStorage.setItem("login", false)
-    window.location.href = "https://grid013.github.io/bikerental/";
-}
+// function logout() {
+//     localStorage.setItem("login", false)
+//     window.location.href = "https://grid013.github.io/bikerental/";
+// }
 
-var navs = document.getElementById("navlist")
-const login = localStorage.getItem("login")
-if (login == "true") {
-    navs.innerHTML += `<li class="nav-item">
-    <a class="nav-link fw-bold" onclick="logout()" role="button">Logout</a>
-</li>`
+// var navs = document.getElementById("navlist")
+// const login = localStorage.getItem("login")
+// if (login == "true") {
+//     navs.innerHTML += `<li class="nav-item">
+//     <a class="nav-link fw-bold" onclick="logout()" role="button">Logout</a>
+// </li>`
 
-}
-else {
-    navs.innerHTML += `<li class="nav-item">
-    <a class="nav-link fw-bold" href="login.html">Login</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link fw-bold" href="register.html">Register</a>
-</li>`
+// }
+// else {
+//     navs.innerHTML += `<li class="nav-item">
+//     <a class="nav-link fw-bold" href="login.html">Login</a>
+// </li>
+// <li class="nav-item">
+//     <a class="nav-link fw-bold" href="register.html">Register</a>
+// </li>`
 
-}
+// }
